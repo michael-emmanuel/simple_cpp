2833d4c443ccec5a967f6b1f55789d5328b30d93
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-11-01 23:26:52 -0400

    and & or

--- source.cpp
+++ source.cpp
@@ -4,25 +4,14 @@ using namespace std;
 
 int main()
  {
-  // complex way to create string using char
-  char MyComplexString[5] = {'D', 'o', 'g', 's', '\0'};
+  int i = 1;
+  int j = 2;
+  int k = 3;
 
-  // less complex way to create string using char 
-  // len 5 to account for null char
-  char myLessComplexString[5] = "Dogs";
-
-  // go to way, must #include string
-  // w/o importing namespace std -> std::string
-  string MyString;
-  MyString = "My dog's name is: ";
-  string first = "Spot ";
-  string last = "Jones";
-
-  MyString += (first + last);
-
-  cout << MyComplexString << endl;
-  cout << myLessComplexString << endl;
-  cout << MyString << endl;
+  if (i <= k || i == j)
+  {
+    cout << "This will be printed" << endl;
+  }
   
   return 0;
  }
