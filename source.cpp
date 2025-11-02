b0e0ab115566413f664a936bb77b3ca62b8a09f8
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-11-02 01:32:32 -0400

    pointers on structs

--- source.cpp
+++ source.cpp
@@ -2,32 +2,26 @@
 #include <string>
 using namespace std;
 
+struct Container
+{
+  string Name;
+
+  int x;
+  int y;
+  int z;
+};
+
 int main()
  {
-  int a = 100;
-  int* aPtr;
-  aPtr = &a; // &a => address of operator, returns address of a NOTE: & on left side of equal sign for reference
+  Container container = {"Sam", 5, 6, 7};
 
-  cout << aPtr << endl;
-  cout << *aPtr << endl; // dereference to get value living in address
+  Container* PtrToCont = &container;
 
-  int numbers[] = {0,1,2,3,4,5,6,7,8,9,10};
+  cout << (*PtrToCont).Name << endl; // must use parenthesis to properly access properties
+  cout << (*PtrToCont).x << endl;
+  cout << (*PtrToCont).y << endl;
+  cout << (*PtrToCont).z << endl;
 
-  int* numPtr = numbers; // only stores the address of the first element
-
-  cout << *numPtr << endl;
-
-  numPtr++; // bc arrays are contigious in memory, incrementing give us next val in array
-
-  cout << *numPtr << endl;
-
-  numPtr++;
-
-  cout << *numPtr << endl;
-
-  numPtr += 2;
-
-  cout << *numPtr << endl;
 
   return 0;
  }
