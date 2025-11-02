502ae308786359b1acf2f069ca7366fe46e6ee15
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-11-02 13:49:36 -0500

    freeing up memory on heap to avoid memory leak

--- source.cpp
+++ source.cpp
@@ -2,84 +2,26 @@
 #include <string>
 using namespace std;
 
-class Creature
+struct Character
 {
-  public:
-    Creature();
-
-    void SetName(string name);
-    string GetName();
-
-    float GetHealth();
-
-    void TakeDamage(float damage);
-
-  private:
-    string Name;
-    float Health;
-
-  protected:
-    int NumberOfLimbs;
-
-};
-
-class Goblin : public Creature
-{
-  Goblin();
+  string Name;
+  float Health;
 };
 
 int main()
 {
-  Creature Igor;
-  Igor.SetName("Igor");
-  cout << "Name: " << Igor.GetName() << endl;
-  cout << "Health: " << Igor.GetHealth() << endl;
+  for (int i = 0; i < 1000000000000000; i++)
+  {
+    Character* PtrToChar = new Character();
 
-  cout << "Igot will now take 35 damage!" << endl;
-  Igor.TakeDamage(35.0);
+    PtrToChar->Name = "Neo";
+
+    cout << PtrToChar->Name << endl;
+
+    delete PtrToChar;
+  }
+
 
   return 0;
 }
 
-Creature::Creature()
-{
-  Health = 100.f;
-  cout << "A creature has been created!\n";
-}
-
-void Creature::SetName(string name)
-{
-  Name = name;
-}
-
-string Creature::GetName()
-{
-  return Name;
-}
-
-float Creature::GetHealth()
-{
-  return Health;
-}
-
-void Creature::TakeDamage(float damage)
-{
-  float Total;
-  Total = Health - damage;
-
-  if (Total <= 0.f) 
-  {
-    cout << GetName() << " has died!\n";
-  } 
-  else 
-  {
-    Health -= damage;
-  }
-
-  cout << "Health: " << Health << endl;
-}
-
-Goblin::Goblin()
-{
-  NumberOfLimbs = 5;
-}
