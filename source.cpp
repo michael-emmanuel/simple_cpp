3a9a85c896238e25b0f55534c5132b83041f4fd6
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-11-01 20:17:30 -0400

    c++ references comments

--- source.cpp
+++ source.cpp
@@ -6,6 +6,15 @@ void add_to_int(int &r)
   r += 5;
 }
 
+// references cannot be null
+
+// you cannot change referenced variable
+// once a var is associated to a reference
+// it is that way forever
+
+// You cannot do math on an index (unlike pointer)
+
+// You cannot point references to references (unlike pointer)
 int main()
   {
     int i = 0;
