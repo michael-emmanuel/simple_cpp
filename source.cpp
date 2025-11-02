e56d3387f199bea45daccd9aa0c47cd6fd9171b0
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-11-02 11:39:26 -0500

    learning classes in c++

--- source.cpp
+++ source.cpp
@@ -2,27 +2,23 @@
 #include <string>
 using namespace std;
 
-struct Container
+class Dog
 {
   string Name;
+  int Age;
+  float Health;
 
-  int x;
-  int y;
-  int z;
+  void Bark()
+  {
+    cout << "Woof!" << endl;
+  }
 };
 
 int main()
  {
-  Container container = {"Sam", 5, 6, 7};
+  Dog Spot;
+  Dog Rex;
 
-  Container* PtrToCont = &container;
-
-  cout << (*PtrToCont).Name << endl; // must use parenthesis to properly access properties
-  cout << (*PtrToCont).x << endl;
-  cout << (*PtrToCont).y << endl;
-  cout << (*PtrToCont).z << endl;
-
-  cout << PtrToCont->Name << endl; // syntactic sugar - better than parenthesis
 
   return 0;
  }
