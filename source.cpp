db3f65285065d85576915ee6fbdf893dde952bd7
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-11-02 00:08:43 -0400

    enums

--- source.cpp
+++ source.cpp
@@ -2,15 +2,35 @@
 #include <string>
 using namespace std;
 
+enum PlayerStatus
+{
+  PS_Crouched, 
+  PS_Standing,
+  PS_Walking,
+  PS_Running
+};
+
+enum MovementStatus
+{
+  MS_Crouched,
+  PS_Running
+}
+
 int main()
  {
-  int MyArray[5] = { 1, 23, 5, 4, 9 };
+  PlayerStatus status;
+  status = PlayerStatus::PS_Running;
 
-  for (int i = 0; i < 5; i++)
+  if (status == PS_Crouched)
   {
-    cout << "MyArray[" << i << "] = " << MyArray[i] << endl;
+    cout << "The player is crouching! \n";
   }
-  
+
+  status = PS_Walking;
+  if (status == PS_Crouched)
+  {
+    cout << "The player is crouching! \n";
+  }
 
   return 0;
  }
