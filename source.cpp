7b30d6d44818f7c475c95d59aac96dd810083c14
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-11-02 01:09:22 -0400

    learning pointers.

--- source.cpp
+++ source.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
  {
   int MyInt = 2;
-
+  // Passing around addresses in a program is more efficient that pass a huge struct or long array
   // pointers do not have to be initialized right away, unlike references
   int* ptr;
   ptr = &MyInt;
