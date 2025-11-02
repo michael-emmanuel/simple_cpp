dfc9972b648929154c5adc9cc38a872c64fae2e3
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-11-02 01:08:15 -0400

    learning pointers

--- source.cpp
+++ source.cpp
@@ -2,65 +2,15 @@
 #include <string>
 using namespace std;
 
-struct LocationVector
-{
-  float X;
-  float Y;
-  float Z;
-};
-
-struct Player 
-{
-  int Level;
-  float Health;
-  float Damage;
-  float stamina;
-
-  LocationVector Location;
-
-  void TakeDamage(float dmg)
-  {
-    Health -= dmg;
-  }
-
-  int GetLevel()
-  {
-    if (Level > 10) 
-    {
-      cout << "Level is greater than 10 \n";
-    }
-    return Level;
-  }
-
-  void DisplayLocation()
-  {
-    cout << "Location.X = " << Location.X << endl;
-    cout << "Location.Y = " << Location.Y << endl;
-    cout << "Location.Z = " << Location.Z << endl;
-  }
-
-};
-
 int main()
  {
-  Player p_1;
-  p_1.Level = 11;
-  p_1.Health = 100.f;
-  p_1.Damage = 10.f;
-  p_1.stamina = 20.f;
+  int MyInt = 2;
 
-  cout << "p_1 Level = " << p_1.GetLevel() << endl;
-
-  p_1.TakeDamage(40.f);
-
-  cout << "p_1 takes " << 40.f << " damage!" << endl;
-  cout << "p_1 Health = " << p_1.Health << endl;
-
-  p_1.DisplayLocation();
-
-  Player p_2 = {1, 50.f, 40.f, 35.54f, {35.5f, 67.45f, 100.003f}};
-
-  p_2.DisplayLocation();
+  // pointers do not have to be initialized right away, unlike references
+  int* ptr;
+  ptr = &MyInt;
+  cout << ptr << endl;
+  cout << *ptr << endl; // the * dereferences the pointer, returning the value stored in memory
 
   return 0;
  }
